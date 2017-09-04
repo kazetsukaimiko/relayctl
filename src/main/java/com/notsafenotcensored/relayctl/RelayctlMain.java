861ba7a4823180fac2bb4a086348958b49c5670b
Scott Serbousek <airetamstrm@gmail.com>
2017-09-03 23:47:41 -0400

    Testable

--- src/main/java/com/notsafenotcensored/relayctl/RelayctlMain.java
+++ src/main/java/com/notsafenotcensored/relayctl/RelayctlMain.java
@@ -1,6 +1,9 @@
 package com.notsafenotcensored.relayctl;
 
 
+import com.notsafenotcensored.relayctl.relay.RelayController;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
 import io.undertow.Undertow;
 import io.undertow.servlet.Servlets;
 import io.undertow.servlet.api.DeploymentInfo;
@@ -16,7 +19,20 @@ public class RelayctlMain {
     public static int getPort() {
         return 8080;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        test();
+    }
+
+    public static void test() throws InterruptedException {
+        RelayController rc = new RelayController();
+        for (GpioPinDigitalOutput gpioPDO : rc.getRelays()) {
+            gpioPDO.setState(PinState.HIGH);
+            Thread.sleep(750);
+        }
+        rc.shutdown();
+    }
+
+    public static void deploy() {
         UndertowJaxrsServer server = new UndertowJaxrsServer();
         Undertow.Builder builder = Undertow.builder()
                 .addHttpListener(getPort(), getBindAddress());
